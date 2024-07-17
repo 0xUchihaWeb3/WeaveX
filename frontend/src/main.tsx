819ac36f79ff2ceedec6d4523dd54f92fb704694
jainv4156 <jainv4156@gmail.com>
2024-07-17 22:18:22 +0530

    jafsdkjsfl

---
+++ frontend/src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './index.css'
+import ArConnectStrategy from "@arweave-wallet-kit/arconnect-strategy";
+import { ArweaveWalletKit } from "@arweave-wallet-kit/react";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ArweaveWalletKit
+      config={{
+        permissions: [
+          "ACCESS_ADDRESS",
+          "ACCESS_PUBLIC_KEY",
+          "SIGN_TRANSACTION",
+          "DISPATCH",
+        ],
+        ensurePermissions: true,
+        strategies: [new ArConnectStrategy()],
+      }}
+      theme={{
+        displayTheme: "dark",
+      }}
+    >
+      <App />
+    </ArweaveWalletKit>
+  </React.StrictMode>
+);
